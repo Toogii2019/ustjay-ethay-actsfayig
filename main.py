1ba3feb87f914259cacc4c05da40202c7894cee9
Toogii2019 <dashdava@uw.edu>
2020-05-03 17:19:34 -0700

    Completed fancier

--- main.py
+++ main.py
@@ -14,12 +14,18 @@ def get_fact():
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
 
-    return facts[0].getText()
+    return facts[0].getText().strip()
 
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    query_args = {'input_text': get_fact()}
+    res = requests.post('https://hidden-journey-62459.herokuapp.com/piglatinize/', query_args)
+    redirected_link = "#"
+    for i in res.history:
+        if i.status_code == 302:
+            redirected_link = i.headers.get('Location')
+    return "<h1><img src='static/Wizard.jpg' width='600' height='400'></h1> \r\n <a href='{0}'>Click here to see magic - {0}</a>".format(redirected_link)
 
 
 if __name__ == "__main__":
